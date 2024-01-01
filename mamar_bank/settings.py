997df29f90923c2bd91a0aa1a7f4fede09a42a73
marjanhasan <moonmarjanhasan@gmail.com>
2024-01-01 23:48:52 +0600

    bug fixed

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "moonmarjanhasan@gmail.com"
-EMAIL_HOST_PASSWORD = "rxbqydaxmrvgxnoh"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
